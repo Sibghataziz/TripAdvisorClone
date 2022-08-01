5b342998639baa7f869fcb798db305dc8826a42c
sibghataziz <sibghat.aziz98@gmail.com>
2022-08-01 22:14:37 +0530

    Initial data-Structure

---
+++ data.js
@@ -0,0 +1,61 @@
+// import data;
+
+data = {
+    "Kokata": {
+        Hotel:  [{
+            name: "The Oberio",
+            image:  [],
+            rating: {   rating: 4.5,
+                        review: 5500,
+                        location: 4.5,
+                        cleanliness:4.5,
+                        service:4.5,
+                        value:4.5},
+            about:`See why so many travellers make ITC Sonar their hotel of choice when visiting Kolkata (Calcutta). Providing an ideal mix of value, comfort and convenience, it offers a romantic setting with an array of amenities designed for travellers like you.
+
+            While staying at Itc Hotel Kolkata, visitors can check out Birla Temple (2.2 mi) and Sri Aurobindo Bhawan (2.2 mi), some of Kolkata (Calcutta)'s top attractions.
+            
+            Guest rooms offer a flat screen TV, a refrigerator, and a minibar, and Itc Sonar Hotel makes getting online easy as free internet access is available.
+            
+            You can also take advantage of some of the amenities offered by the hotel, including 24 hour front desk, room service, and a concierge. In addition, guests can enjoy a pool and breakfast during their visit. As an added convenience, there is free parking available to guests.
+            
+            Travellers looking to enjoy some shrimp can head to Barbeque Nation, Mainland China, or Kewpie's. Otherwise, you may want to check out a Mediterranean restaurant such as Whats Up! Cafe, Mio Amore, or Cafe Mezzuna.
+            
+            Kolkata (Calcutta) is also known for some great history museums, including Indian Museum (Jadu Ghar), Swami Vivekananda's Ancestral House, and Kolkata Tram Museum Smaranika, which are not too far from Itc Sonar Hotel Kolkata.
+            
+            At ITC Sonar, your comfort and satisfaction come first, and they look forward to welcoming you to Kolkata (Calcutta).`,
+            hotel_link : "link"
+        }],
+        Eat:    {},
+        ToDo:   {},
+        Travel: {}
+    },
+    "Delhi" : {
+        Hotel:  {},
+        Eat:    {},
+        ToDo:   {},
+        Travel: {}
+    },
+    "Mumbai": {
+        Hotel:  {},
+        Eat:    {},
+        ToDo:   {},
+        Travel: {}
+    },
+    "Chennai": {
+        Hotel:  {},
+        Eat:    {},
+        ToDo:   {},
+        Travel: {}
+    },
+}
+
+
+
+// "kolkata" = {k:11,O:15,l:13,a:1,t:21} = 11+15+13+11+1+21+1 = 78
+// Chennai                                                     = 90
+//                                                                 17
+//                                                                 213*40
+
+
+
