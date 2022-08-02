3e226290e8faf8b01ccb03b972c0a69ac9476a6f
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2022-08-02 18:48:38 +0530

    added data in thingsToDo file

---
+++ thingsToDo.js
@@ -0,0 +1,233 @@
+
+var places = [ "Mumbai" , "Kolkata" , "Delhi" , "Chennai" ] ;
+
+var thingsToDo = [ 
+       {    name :  "City Tours" ,
+            image : "https://media.tacdn.com/media/attractions-splice-spp-360x240/06/6a/c4/f0.jpg" 
+        
+        }  ,
+         {
+            name : "Shopping Malls" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/6e/86/2a/little-mall.jpg?w=300&h=300" 
+        } , 
+        {
+            name : "Cultural Tours" ,
+            image : "https://media.tacdn.com/media/attractions-splice-spp-360x240/07/bd/a1/c4.jpg" 
+        } ,
+        {
+            name : "Night Tours" ,
+            image : "https://media.tacdn.com/media/attractions-splice-spp-360x240/07/93/b2/ab.jpg" 
+        } ,
+        {
+            name : "parks" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/1d/7a/b9/img-20170801-wa0678-largejpg.jpg?w=300&h=300" 
+        } ,
+        {
+            name : "Historical Tours" ,
+            image : "https://media.tacdn.com/media/attractions-splice-spp-360x240/06/71/ad/ca.jpg" 
+        }
+    ]
+
+    var cityTours = [
+        {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        } , {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        } , {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        } , {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        } , {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        } , {
+            title : "Highlights of Mumbai Private Sightseeing Tour:Winner of TRAVELLERS CHOICE AWARD" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/64/5f/f1/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.5" ,
+            recommendation : "99% of the travellers"
+        }
+    ]
+
+    var shoppingMall = [
+        {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        } , {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        } , {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        } , {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        } , {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        } , {
+            title : "Viviana Mall",
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/77/66/22/viviana-mall-image-taken.jpg?w=500&h=400&s=1" ,
+            ratings : "4.0" ,
+            recommendation : "97% of the travellers" 
+        }
+    ]
+
+    var culturalTours = [
+        {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        } , {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        } , {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        } , {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        } , {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        } , {
+            title : "Best of Mumbai by Night Private Tour with Dinner and Transport" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/a1/14/b3/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "95% of the travellers" 
+        }
+    ]
+
+    var nightTours = [
+        {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        } , {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        } , {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        } , {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        } , {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        } , {
+            title : "Private Mumbai At Night 4-Hour Tour" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/57/18/25/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.7" ,
+            recommendation : "99% of the travellers" 
+        }
+    ]
+
+    var parks = [
+        {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Priyadarshini Park" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/e6/79/8f/priyadarshini-park.jpg?w=500&h=-1&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        }
+    ]
+
+    var historicalTours = [
+        {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        } , {
+            title : "Private Dharavi Slum Tour with Mumbai Sightseeing" ,
+            image : "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/35/cd/91/caption.jpg?w=500&h=400&s=1" ,
+            ratings : "4.2" ,
+            recommendation : "92% of the travellers" 
+        }
+    ]
