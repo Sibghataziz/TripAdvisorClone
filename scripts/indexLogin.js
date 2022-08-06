ecfaa4ab445e9741f3c76082b54500d84ec22ac1
sibghataziz <sibghat.aziz98@gmail.com>
2022-08-06 21:24:31 +0530

    Travel8

---
+++ scripts/indexLogin.js
@@ -0,0 +1,20 @@
+let user = localStorage.getItem("tripUser");
+
+const div = document.querySelector(".list>ul>a");
+
+// console.log(div)
+if (user) {
+  div.innerText = "";
+  const btn = document.createElement("button");
+  btn.innerText = "Log Out";
+  btn.addEventListener("click", logoutUser);
+  btn.setAttribute("id", "signin");
+
+  div.append(btn);
+  div.removeAttribute("href");
+}
+
+function logoutUser() {
+  localStorage.removeItem("tripUser");
+  location = "index.html";
+}
